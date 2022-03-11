ac2cc65e0b84c1329a69b7bac49484e4a6bb176a
kennycha <dudqn136@naver.com>
2022-03-11 18:42:54 +0900

    feat: add a control panel where theme state can be toggled

---
+++ src/containers/panels/ControlPanel/index.tsx
@@ -0,0 +1,33 @@
+import { FunctionComponent, memo, useCallback } from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+import { useSelector } from "../../../reducers";
+import * as themeActions from '../../../actions/theme'
+
+interface Props {}
+
+const ControlPanel: FunctionComponent<Props> = () => {
+  const theme = useSelector((state) => state.theme)
+  const dispatch = useDispatch()
+  
+  const handleToggleThemeColor = useCallback(() => {
+    if (theme.color === 'dark') {
+      dispatch(themeActions.changeTheme({ color: 'light' }))
+    } else {
+      dispatch(themeActions.changeTheme({ color: 'dark' }))
+    }
+  }, [dispatch, theme.color])
+
+  return (
+    <Container>
+      <button onClick={handleToggleThemeColor}>{theme.color}</button>
+    </Container>
+  )
+}
+
+export default memo(ControlPanel)
+
+const Container = styled.section`
+  width: 100%;
+  height: 100%;
+`
